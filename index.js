b131735ff743330ea549f7dac79ed69d79a5c059
spookeyy <pangasmeshack@gmail.com>
2024-03-23 22:45:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards( namesArray, eventName ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!` )
+    }
+    return thankYouCards
+  }
+  
+  function countDown(startNo) {
+    while ( startNo > 0 ) {
+      console.log(startNo);
+      startNo--;
+    }
+    console.log(startNo);
+  }
